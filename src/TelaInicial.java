254f3e05212315a883f44632fc21f6433fc30409
Samuel William <swzubek@hotmail.com>
2024-11-10 20:40:18 -0300

    Tela inicial definida

---
+++ src/TelaInicial.java
@@ -0,0 +1,15 @@
+
+import view.cadastroVIEW;
+
+
+public class TelaInicial {
+    
+    public static void main(String args[]) {
+        
+        cadastroVIEW tela = new cadastroVIEW();
+        tela.setVisible(true);
+        
+    }
+    
+    
+}
