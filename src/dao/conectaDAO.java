886ca68234e24000e6503b57ceec3e246f09d8d6
Samuel William <swzubek@hotmail.com>
2024-11-10 21:28:17 -0300

    Alterada as credencias de conexão MySQL

--- src/dao/conectaDAO.java
+++ src/dao/conectaDAO.java
@@ -13,7 +13,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11", "root", "root");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
